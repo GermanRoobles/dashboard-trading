414c3719534d2775de1d183005ac069fa829c693
German Robles <germanroobles@gmail.com>
2025-03-13 12:39:22 +0100

    subida

--- apps/run_interface.py
+++ apps/run_interface.py
@@ -35,6 +35,7 @@ def main():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
     app = create_dashboard(port=port)
+    server = app.server  # This line is important for gunicorn
     app.run_server(
         host='0.0.0.0',
         port=port,
